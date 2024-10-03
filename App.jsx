8561036cb2b1e130fe9a7ddc62fe66774e42c047
srinitha709 <121370607+srinitha709@users.noreply.github.com>
2024-10-03 15:45:54 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import hook from './components/hook.jsx' 
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <hook/>
+    
+
+    </>
+  )
+}
+
+export default App
